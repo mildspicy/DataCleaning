02c5dbfa8739b7f8d66439eeb2372c8dccb6933f
mildspciy <jiangqinglong2020@outlook.com>
2023-05-08 17:59:19 +0800

    增加一些功能

--- DataSplit/efficiency.py
+++ DataSplit/efficiency.py
@@ -1,86 +1,93 @@
 import json
 import os
+import shutil
 
+from config import config as cfg
 import pandas
 
 
-class Efficiency:
-    def __init__(self):
-        self.name = None
+def get_config():
+    with open('./DataSplit/config.json', 'r', encoding='utf-8') as f:
+        config = json.load(f)
+    return config
 
-    def get_config(self):
-        with open('./DataSplit/config.json', 'r', encoding='utf-8') as f:
-            config = json.load(f)
-        return config[self.name]
 
-    def execute(self, name):
-        self.name = name
-        config = self.get_config()
-        print(config)
-        sp: SplitData = SplitData(config)
-        sp.start_split()
+def execute():
+    config = get_config()
+    sp: SplitData = SplitData()
+    sp.start_split(config)
 
 
 class SplitData:
-    def __init__(self, config):
+    def __init__(self):
         # 根路径
-        self.rootpath = config['rootpath']
-        # 项目文件夹名
-        self.name = config['out_dir']
+        self.out_path = cfg.OUT_PATH
         # 数据源路径
-        self.source_files = config['source_files']
-        # 需要处理的数据表
-        self.split_files = config['split_files']
-        # 数据范围
-        self.start_date = config['start_date']
-        self.end_date = config['end_date']
+        self.source_files = cfg.SOURCE_PATH
         # 结果数据集
         self.result = []
         # 筛选条件
-        self.config = config
+        self.config = None
+        # 历史文件路径
+        self.move_path = cfg.MOVE_PATH
 
-    def start_split(self):
-        for target_file in self.split_files:
-            print(target_file)
-            for file in os.listdir(self.source_files):
-                print(file)
-                if file.startswith(target_file):
-                    full_path = '{}/{}'.format(self.source_files, file)
-                    pd = pandas.read_excel(full_path, engine='openpyxl')
-                    filter_pd = self.filter_data(target_file, pd)
+    def start_split(self, config):
+        # 循环文件
+        for file in os.listdir(self.source_files):
+            print(file)
+            full_path = '{}/{}'.format(self.source_files, file)
+            pd = pandas.read_excel(full_path, engine='openpyxl')
+            update_date = pd['统计日期'].drop_duplicates().tolist()
+            # 获取文件类型名
+            file_category = file[:2]
+            for project in config:
+                self.config = config[project]
+                if file_category in config[project]['split_files']:
+                    filter_pd = self.filter_data(file_category, pd)
                     self.result.append(filter_pd)
-            self.save_data(target_file)
-            self.result = []
+                self.save_data(file_category, update_date)
+                self.result = []
+                print('{}__{}成功'.format(file, project))
+            move_path = '{}/{}'.format(self.move_path, file)
+            shutil.move(full_path, move_path)
+            print('移动文件成功！！！！')
 
-    def filter_data(self, target_file, pd):
-        if target_file == "效能":
+    def filter_data(self, file_category, pd):
+        if file_category == "效能":
             filter_efficiency = self.config["filter_efficiency"]
             for row in filter_efficiency:
                 if row == "人工接通量":
                     pd = pd.loc[(pd[row] > filter_efficiency[row])]
                 else:
-                    pd = pd.loc[(pd[row] == filter_efficiency[row])]
-        elif target_file == "场景":
+                    pd = pd[pd.apply(lambda x: x[row] in filter_efficiency[row], axis=1)]
+        elif file_category == "场景":
             filter_efficiency = self.config["filter_scene"]
             for row in filter_efficiency:
                 if row == "人工接通量":
                     pd = pd.loc[(pd[row] > filter_efficiency[row])]
                 else:
-                    pd = pd.loc[(pd[row] == filter_efficiency[row])]
+                    pd = pd[pd.apply(lambda x: x[row] in filter_efficiency[row], axis=1)]
+        print("filter success!!!!!")
         return pd
 
-    def save_data(self, target_file):
-        out_full_path = '{}/{}/{}.xlsx'.format(self.rootpath, self.name, target_file)
-        con = pandas.concat(self.result, ignore_index=False)
-        con.to_excel(out_full_path, index=False)
+    def save_data(self, file_category, update_date):
+        out_full_path = '{}/{}/{}.xlsx'.format(self.out_path, self.config['out_dir'],
+                                               self.config[file_category]['file_name'])
+        if os.path.exists(out_full_path):
+            pd = pandas.read_excel(out_full_path, engine='openpyxl',
+                                   sheet_name=self.config[file_category]['out_sheet_name'])
+            result_date = pd['统计日期'].drop_duplicates().tolist()
+            if update_date in result_date:
+                for row in update_date:
+                    pd = pd.loc[(pd['统计日期'] != row)]
+            pd_result = pandas.DataFrame(self.result[0])
+            con = pandas.concat([pd, pd_result], ignore_index=False)
+        else:
+            con = pandas.concat(self.result, ignore_index=False)
+        con.to_excel(out_full_path, index=False, sheet_name=self.config[file_category]['out_sheet_name'],
+                     engine='openpyxl')
+        print("Export excel Success !!!!!!!!!")
 
 
 if __name__ == '__main__':
-    Efficiency = Efficiency()
-    tables = [
-        "北京",
-        # "广西"
-    ]
-    for name in tables:
-        Efficiency.execute(name)
-
+    execute()
--- test.py
+++ test.py
@@ -1,7 +1,46 @@
-import pandas
+import os
 
-pd = pandas.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns=['a','b','c'])
-print(pd)
-print('________________________________')
-pd = pd.loc[(pd['a'] == 1)]
-print(pd)
+import pandas
+# pd = pandas.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns=['a','b','c'])
+# print(pd)
+# print('________________________________')
+# pd = pd.loc[(pd['a'] == 1)]
+# print(pd)
+
+
+# str = '效能1月.xlsx'
+# print(str[:2])
+
+
+# if os.path.exists(r'C:\Users\Administrator\Desktop\Python\DataCleaning\file\场景1月.xlsx'):
+#     print(1)
+# else:
+#     print(2)
+
+
+# os.chdir(r'C:\Users\Administrator\Desktop\Python\DataCleaning\file\场景1月.xlsx')
+
+fil = {'20230101', '20230102', '20230103'}
+# fil = [20230101, 20230102, 20230103]
+# data = []
+pd = pandas.read_excel(r'./test_file/日期数据.xlsx', engine='openpyxl', dtype=str)
+pds = pd.apply(lambda x: x['日期'] in fil, axis=1)
+print(pd[pds])
+
+# data.append(pd)
+
+# con = pandas.concat(data, ignore_index=False)
+# con.to_excel(r'./test_file/file/', index=False, )
+
+# pd = pd[pd.groupby['日期']['日期'].transform(lambda x: set(x) == fil)]
+# pd = pd[pd..isin(fil)]
+# print(pd.columns)
+# pd = pd.groupby('日期')
+# print(pd)
+
+# df = pd.DataFrame({'project_id': [36423, 28564, 96648, 96648, 10042, 68277, 68277, 68277], 'codename': ['banana', 'apple', 'peach', 'peach', 'melon', 'pear', 'pear', 'pear'], 'studio': ['paris', 'amsterdam', 'frankfurt', 'paris', 'london', 'brussel', 'amsterdam', 'sofia']})
+# reqd_studios = {'frankfurt', 'paris'}
+# # idx_reqd_studios = df.apply(lambda x:  reqd_studios.issubset(set(df[df.project_id == x.project_id].studio)), axis=1)
+# idx_reqd_studios = df.apply(lambda x:   x.studio in reqd_studios, axis=1)
+# print(df[idx_reqd_studios])
+
